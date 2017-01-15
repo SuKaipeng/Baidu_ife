dd603901c81f5eab73f24e91302169c5f72cf4d4
SuKaipeng <sukaipeng@yahoo.com>
2017-01-16 00:07:20 +0800

    first draft done

--- 2016_spring/task_22/action.js
+++ 2016_spring/task_22/action.js
@@ -2,23 +2,41 @@ var box1 = Array.prototype.slice.call(document.getElementsByClassName("box1"), 0
 var box2 = Array.prototype.slice.call(document.getElementsByClassName("box2"), 0);
 var box3 = Array.prototype.slice.call(document.getElementsByClassName("box3"), 0);
 var box4 = Array.prototype.slice.call(document.getElementsByClassName("box4"), 0);
-var allBoxes = box1.concat(box2).concat(box3).concat(box4);
-/*
-function highlight(box){
-	box.style = "background-color:red;"
+var allBoxes = box1.concat(box2).concat(box3).concat(box4); //按顺序存储结构存储的div数组
+var array = []; //存放先序遍历后的div
+
+function preOrderTraverse(x){
+	array.push(allBoxes[x]);
+	if (allBoxes[2 * x + 1]) preOrderTraverse(2 * x + 1);
+	if (allBoxes[2 * x + 2]) preOrderTraverse(2 * x + 2);
 }
-*/
-function inOrderTraverse(x, visit){
-		console.log(x);
-		visit(allBoxes[x]);
-		if (allBoxes[2 * x + 1]) inOrderTraverse(2 * x + 1, visit);
-		if (allBoxes[2 * x + 2]) inOrderTraverse(2 * x + 2, visit);
+
+function animation(){
+	var i = 0;
+	array[i].style.backgroundColor = "red";
+	setTimeout(function(){
+		if (i == array.length - 1){
+			array[array.length - 1].style.backgroundColor = "white";
+		} else {
+			i++;
+			console.log(i);
+			array[i].style.backgroundColor = "red";
+			array[i-1].style.backgroundColor = "white";
+			setTimeout(arguments.callee, 500);
+		}
+	}, 500);
+}
+
+function zero(){
+	array = [];
 }
 
 function init(){
 	var btn = document.getElementById("inOrder");
 	btn.addEventListener("click", function(){
-		a();
+		zero();
+		preOrderTraverse(0);
+		animation();
 	}, false);
 }
 
