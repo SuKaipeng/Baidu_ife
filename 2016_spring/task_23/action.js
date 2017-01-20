4b5a2970e7090a3e7a3ab3256f43f6f048184efa
US <us@USs-MacBook-Air-3.local>
2017-01-20 01:16:05 -0800

    Done

--- 2016_spring/task_23/action.js
+++ 2016_spring/task_23/action.js
@@ -10,7 +10,6 @@ MultiTree.prototype.rDFS = function(node){
 		this.rDFS(w);
 		w = w.nextElementSibling;
 	}
-	console.log(this.order);
 }
 //广度优先遍历（递归）（19行的做法）
 MultiTree.prototype.rBFS = function(node){
@@ -20,15 +19,18 @@ MultiTree.prototype.rBFS = function(node){
 		node = this.order[this.rBFSindex++]; //18行的函数递归到底，再执行19行，使node返回为this.order中第一个元素
 		this.rBFS(node.firstElementChild);
 	}
-	console.log(this.order);
 }
 
-MultiTree.prototype.animation = function(order){
+MultiTree.prototype.animation = function(order, search){
 	var i = 0;
+	//清除所有元素的样式
+	for (var j = 0; j < order.length; j++){
+		order[j].style.backgroundColor = "white";
+	}
 	order[i].style.backgroundColor = "red";
 	setTimeout(function(){
 		if (i === (order.length - 1)){
-			order[i].style.backgroundColor = "white";
+			if (!search) order[i].style.backgroundColor = "white";
 		} else {
 			i++;
 			order[i-1].style.backgroundColor = "white";
@@ -37,14 +39,21 @@ MultiTree.prototype.animation = function(order){
 		}
 	}, 300);
 }
-
+//记录搜索路径
 MultiTree.prototype.searching = function(){
 	var input = document.forms[0].elements[2].value;
-	var re = new RegExp(input, "i");
+	var re = new RegExp("^" + input + "$", "i");
 	var order = [];
-	for (var i = 0; i < this.order; i++){
-		if (re.test(this.order[i])) return order = this.order.slice(0, i+1);
+	for (var i = 0; i < this.order.length; i++){
+		if (re.test(this.order[i].firstChild.nodeValue.trim())) return order = this.order.slice(0, i+1);
 	}
+	console.log(order);
+	return order;
+}
+//清除数据
+MultiTree.prototype.reset = function(){
+	this.order = [];
+	this.rBFSindex = 0;
 }
 
 function init(){
@@ -52,15 +61,27 @@ function init(){
 	var root = document.getElementsByClassName("one")[0];
 	var form = document.forms[0];
 	form.elements[0].addEventListener("click", function(){
-		boxes.order = [];
+		boxes.reset();
 		boxes.rDFS(root);
-		boxes.animation(boxes.order);
+		boxes.animation(boxes.order, false);
 	}, false);
 	form.elements[1].addEventListener("click", function(){
-		boxes.order = [];
+		boxes.reset();
 		boxes.rBFS(root);
-		boxes.animation(boxes.order);
+		boxes.animation(boxes.order, false);
 	}, false);
+	form.elements[3].addEventListener("click", function(){
+		boxes.reset();
+		boxes.rDFS(root);
+		var searchOrder = boxes.searching();
+		boxes.animation(searchOrder, true);
+	});
+	form.elements[4].addEventListener("click", function(){
+		boxes.reset();
+		boxes.rBFS(root);
+		var searchOrder = boxes.searching();
+		boxes.animation(searchOrder, true);
+	});
 }
 
 init();
