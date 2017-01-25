3ba3d8269791adb92d1abf6e743eac95d8149ef4
SuKaipeng <sukaipeng@yahoo.com>
2017-01-26 01:19:17 +0800

    coding

--- 2016_spring/task_25/action.js
+++ 2016_spring/task_25/action.js
@@ -1,18 +1,37 @@
 var root = document.querySelector("ul");
+var selected;
+
+function deepthFirstTraverse(){
+	var order = [],
+	    current;
+	order.push(root);
+	current = root.firstElementChild;
+	while (current){
+		deepthFirstTraverse(current);
+		current = current.nextElementSibling;
+	}
+	return order;
+}
 
 function init(){
+	//折叠展开
 	root.addEventListener("click", function(){
 		var target = event.target;
-		var subMenu = target.firstElementChild;
-		if (target.tagName.toLowerCase() === "li"){
-			if (subMenu.style.display === "none"){
-				subMenu.style.display = "block";
-			} else {
-				subMenu.style.display = "none";
+		var subMenu = target.parentNode.lastElementChild;
+		if (target.tagName.toLowerCase() === "span"){
+			
+			target.style.background = "red";
+			if (target.parentNode.childElementCount > 1){
+				if (subMenu.style.display === "none"){
+					subMenu.style.display = "block";
+				} else {
+					subMenu.style.display = "none";
+				}
 			}
 		}
 		console.log(subMenu.style.display);
 	}, false);
+
 }
 
 init();
